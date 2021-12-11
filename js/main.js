ccecac3ff77e796d962e1a0d488509e095928881
Your Name <you@example.com>
2021-12-11 23:41:07 +0100

    js

---
+++ js/main.js
@@ -0,0 +1,14 @@
+let firstNumber= 4
+console.log(firstNumber)
+
+let secondNumber= 5
+console.log(secondNumber)
+
+function add(firstNumber, secondNumber){
+  let result = firstNumber+secondNumber;
+  return result;
+}
+
+console.log(add (firstNumber, secondNumber));
+let wynikDodawania = add(firstNumber, secondNumber)
+console.log (wynikDodawania);
